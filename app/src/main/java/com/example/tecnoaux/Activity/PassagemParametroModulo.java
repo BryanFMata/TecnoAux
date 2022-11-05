4ed62c08a1d821c27b4999c6a15b90b1b62765ed
BryanFMata <bryan.mata222@gmail.com>
2022-11-05 01:31:39 -0300

    Adicionados todos os textos.

---
+++ app/src/main/java/com/example/tecnoaux/Activity/PassagemParametroModulo.java
@@ -0,0 +1,16 @@
+package com.example.tecnoaux.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tecnoaux.R;
+
+public class PassagemParametroModulo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_passagem_parametro_modulo);
+    }
+}
